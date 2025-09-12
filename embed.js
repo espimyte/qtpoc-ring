04a72402eda255358ad2aa8bcc98e2a07ed3b8c2
Espy <espimyte@gmail.com>
2025-09-12 00:51:10 -0700

    Update embed.js

--- embed.js
+++ embed.js
@@ -17,35 +17,35 @@ class QTPOCWebring extends HTMLElement {
                         align-items: center;
                     "
                 >
-                    <img src="/assets/widget-simple.png" />
+                    <a href="https://qtpoc-ring.netlify.app"><img src="/assets/widget-simple.png" /></a>
                     <div>
-                        <a href="https://qpoc-ring.netlify.app/previous">&lt;&lt; prev</a>
-                        <a href="https://qpoc-ring.netlify.app/random">random</a>
-                        <a href="https://qpoc-ring.netlify.app/next">next &gt;&gt;</a>
+                        <a href="https://qtpoc-ring.netlify.app/previous">&lt;&lt; prev</a>
+                        <a href="https://qtpoc-ring.netlify.app/random">random</a>
+                        <a href="https://qtpoc-ring.netlify.app/next">next &gt;&gt;</a>
                     </div>
                 </div>
                 `
         }
         return `
             <div style="display: flex; align-items: center; gap: 5px">
-                <a href="https://qpoc-ring.netlify.app/previous"
+                <a href="https://qtpoc-ring.netlify.app/previous"
                     ><img src="/assets/widget-arrow-left.png"
                 /></a>
 
-                <a href="https://qpoc-ring.netlify.app">
+                <a href="https://qtpoc-ring.netlify.app">
                     <div style="position: relative">
-                        <a href="https://qpoc-ring.netlify.app"
+                        <a href="https://qtpoc-ring.netlify.app"
                             ><img src="/assets/widget.png"
                         /></a>
                         <a
                             style="position: absolute; right: 0px; top: 0px"
-                            href="https://qpoc-ring.netlify.app/random"
+                            href="https://qtpoc-ring.netlify.app/random"
                         >
                             <img src="/assets/widget-random.png" />
                         </a>
                     </div>
                 </a>
-                <a href="https://qpoc-ring.netlify.app/next"
+                <a href="https://qtpoc-ring.netlify.app/next"
                     ><img src="/assets/widget-arrow-right.png"
                 /></a>
             </div>
