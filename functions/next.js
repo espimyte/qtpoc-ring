20662d696a40d2081416f25b0807722d27a1e960
Espy <espimyte@gmail.com>
2025-09-12 22:58:58 -0700

    Update next.js

--- functions/next.js
+++ functions/next.js
@@ -2,13 +2,6 @@ import { redirect, getNext, getRandom } from './common/utils.js'
 
 exports.handler = function(event, context, callback) {
     const { referer } = event.headers
-    try {
-        console.log("url: "+event.url);
-        console.log("path: "+event.path);
-        console.log(new URL(event.url).searchParams.get('via'));
-    } catch {
-        console.log("Via param not found.")
-    }
     const site = getNext(referer) || getRandom()
 
     callback(null, redirect(site))
