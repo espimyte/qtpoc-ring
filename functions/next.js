70846eb966a03b376bea867cb21a5af4cc72420f
Espy <espimyte@gmail.com>
2025-09-12 22:40:06 -0700

    Update next.js

--- functions/next.js
+++ functions/next.js
@@ -2,6 +2,7 @@ import { redirect, getNext, getRandom } from './common/utils'
 
 exports.handler = function(event, context, callback) {
     const { referer } = event.headers
+    console.log(event.path)
     const site = getNext(referer) || getRandom()
 
     callback(null, redirect(site))
